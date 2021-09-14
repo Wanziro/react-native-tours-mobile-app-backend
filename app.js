bf6ba6002f7ea0745145af89680346070ae96214
Fabrice Niyongabo <ninga.fab@gmail.com>
2021-09-15 00:03:44 +0200

    created user api

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const dbConfig = require("./config/db");
 
 let toursTable = require("./models/tours");
+let usersTable = require("./models/users");
 
 //connecting to db
 mongoose.connect(dbConfig.database, {
@@ -58,6 +59,34 @@ app.get("/api/home/tours", (req, res) => {
     .limit(5)
     .select("title images price currency")
     .sort({ date: "desc" });
+});
+
+app.post("/api/login", (req, res) => {
+  let query = { email: req.body.email, password: req.body.password };
+  usersTable.find(query, (err, user) => {
+    if (err) return err;
+    let message;
+    if (user.length !== 1) {
+      message = {
+        success: false,
+        msg: "Invalid username or password",
+      };
+    } else {
+      message = {
+        success: true,
+        msg: "Logged in successfull",
+        user: [
+          {
+            email: user[0].email,
+            name: user[0].name,
+            address: user[0].address,
+            phone: user[0].phone,
+          },
+        ],
+      };
+    }
+    res.json(message);
+  });
 });
 
 app.listen(8080, () => {
---
+++ models/users.js
@@ -0,0 +1,19 @@
+let mongoose = require("mongoose");
+let today = new Date();
+let y = today.getFullYear();
+let m = String(today.getMonth() + 1).padStart(2, "0");
+let d = String(today.getDay()).padStart(2, "0");
+let date = y + "-" + m + "-" + d;
+
+//creating article schema (structure of students collaction)
+let usersSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: [],
+  address: [],
+  phone: [],
+  date: { type: String, default: date },
+});
+
+module.exports = mongoose.model("users", usersSchema, "users");
+//mongoose.model('model name', schema_variable, 'collection name');
